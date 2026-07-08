636be27c2ef7f02d2e59cbc11d48fb1a5fd8b62f
Harsh jain <harshjn782@gmail.com>
2026-07-08 16:06:10 +0530

    Implement counter functionality with limits and styling
    Added event listeners for increment, decrement, and reset actions. Implemented count limits and dynamic styling based on count value.

--- 01-Counter-App.js
+++ 01-Counter-App.js
@@ -1,1 +1,46 @@
+let dec = document.querySelector('.decrease');
+let inc = document.querySelector('.increase');
+let reset = document.querySelector('.reset');
+let counter = document.querySelector('.counter');
 
+let count = 0;
+
+const MAX = 10;
+const MIN = -10;
+
+inc.addEventListener('click', ()=>{
+  if (count < MAX){
+    count++;
+  }
+  updateCount();
+});
+
+dec.addEventListener('click', ()=>{
+  if (count > MIN) count--;
+  updateCount();
+});
+
+reset.addEventListener('click', ()=>{
+    count = 0;
+    updateCount();
+});
+
+function updateCount(){
+  counter.textContent = count;
+  
+  if(count > 0) counter.style.color = "Green";
+  else if(count === 0) counter.style.color = "Black";
+  else {
+    counter.style.color = "Red";
+  }
+  
+  if (count === MAX)
+    inc.disabled = true;
+  else
+    inc.disabled = false;
+  
+  if (count === MIN)
+    dec.disabled = true;
+  else
+    dec.disabled = false;
+}
